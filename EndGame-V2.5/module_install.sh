fbfb6fd8648e1573ec7a76a8dcd6317495c296f0
Ihor <putalentman@gmail.com>
2023-03-28 21:07:12 +0300

    module added

---
+++ EndGame-V2.5/module_install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/yorkane/socks-nginx-module.git
+git clone https://github.com/nbs-system/naxsi.git
+git clone https://github.com/openresty/headers-more-nginx-module.git
+git clone https://github.com/openresty/echo-nginx-module.git
+git clone https://github.com/openresty/lua-nginx-module
